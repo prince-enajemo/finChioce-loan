984bd847717a1419393401138a1f946ccfdd2d19
prince-morage <princeenajemo@gmail.com>
2025-10-30 23:10:32 +0100

    g

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -54,7 +54,10 @@ export const Navbar = () => {
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <Logo />
-            <p className="font-bold text-inherit"> <span className="text-blue-500">FIN</span>CHIOCE</p>
+            <p className="font-bold text-inherit">
+              {" "}
+              <span className="text-blue-500">FIN</span>CHIOCE
+            </p>
           </NextLink>
         </NavbarBrand>
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
@@ -93,17 +96,16 @@ export const Navbar = () => {
         </NavbarItem>
         {/* <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem> */}
         <NavbarItem className="hidden md:flex">
-        <Button
-  isExternal
-  as={Link}
-  className="text-sm font-bold text-default-600 bg-default-100"
-  href="/register"
-  // startContent={<HeartFilledIcon className="text-danger" />}
-  variant="flat"
->
-  REGISTER
-</Button>
-
+          <Button
+            isExternal
+            as={Link}
+            className="text-sm font-bold text-default-600 bg-default-100"
+            href="/register"
+            // startContent={<HeartFilledIcon className="text-danger" />}
+            variant="flat"
+          >
+            REGISTER
+          </Button>
         </NavbarItem>
       </NavbarContent>
 
