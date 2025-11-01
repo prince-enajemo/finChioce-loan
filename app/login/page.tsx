bb53383e79e6ec7169997026c7724f8c8b768ddd
prince-morage <princeenajemo@gmail.com>
2025-11-01 21:17:35 +0100

    g

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -109,7 +109,7 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="bg-gradient-to-b from-gray-600 to-black flex justify-center items-center h-screen w-screen">
+    <div className="bg-gradient-to-b from-gray-600 to-black flex justify-center items-center h-screen w-full">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-3xl font-bold text-center mb-6 text-gray-800">
           Login
